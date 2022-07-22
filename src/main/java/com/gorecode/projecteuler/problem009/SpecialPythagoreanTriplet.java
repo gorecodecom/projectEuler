1be510ed7ca415f8816ce211d5179cd8485eb54e
Heiko Goretzki <heikogoretzki@gmail.com>
2022-07-22 10:45:38 +0200

    solved problem 9

--- src/main/java/com/gorecode/projecteuler/problem009/SpecialPythagoreanTriplet.java
+++ src/main/java/com/gorecode/projecteuler/problem009/SpecialPythagoreanTriplet.java
@@ -1,4 +1,20 @@
 package com.gorecode.projecteuler.problem009;
 
 public class SpecialPythagoreanTriplet {
+
+    public static void main(String[] args) {
+        String temp = "";
+
+        for (int a = 1; a < 1000; a++) {
+            for (int b = a + 1; b < 1000; b++) {
+                int c = 1000 - a - b;
+                if (a * a + b * b == c * c) {
+                    temp = Integer.toString(a * b * c);
+                }
+            }
+        }
+
+        System.out.println(temp);
+    }
+
 }
